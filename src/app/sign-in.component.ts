6aa780268e4b5d6e327603e2c79a1c6180922613
tuanlm <tuanlmst02@gmail.com>
2023-04-17 12:01:53 +0700

    update ngForm

--- src/app/sign-in.component.ts
+++ src/app/sign-in.component.ts
@@ -3,19 +3,22 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-sign-in',
     template: `
-    <input placeholder="Email" [(ngModel)]="email" />
-    <br><br>
-    <input type="password" placeholder="Password" [(ngModel)]="password" />
-    <br><br>
-    <button (click)="onSubmit()">Submit</button>`,
+            <form (submit)="onSubmit(FormSubmit)" #FormSubmit="ngForm">
+                <input type="text" placeholder="Input your email" [(ngModel)]="email" name="email" />
+                <input type="password" placeholder="Input your password" [(ngModel)]="password" name="password" />
+                <br/><br/>
+                <button>Click me</button>
+            </form>
+            `,
 })
 
 export class SignInComponent {
     email = '';
     password = '';
 
-    onSubmit() {
+    onSubmit(FormSubmit: any) {
         console.log(this.email);
         console.log(this.password);
+        console.log(FormSubmit.value);
     }
 }
